e75c7e6c0e89cacfb54fb4c184745697af14343a
Jimmy Israel <djimmyisrael@hotmail.com>
2022-05-03 16:34:02 -0500

    Rutas API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PersonsController;
+use App\Http\Controllers\Api\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('persons', PersonsController::class);
+Route::resource('personDetail', ProfileController::class);
